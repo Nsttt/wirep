258d50c140209255a2eee1ed72f36ebe441285de
Nestor <nestor@nstlopez.com>
2024-03-02 13:53:59 +0100

    Create proxy gateway

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,122 @@
 use async_trait::async_trait;
-use pingora::prelude::*;
-use std::sync::Arc;
+use log::info;
+use prometheus::register_int_counter;
+use structopt::StructOpt;
+
+use pingora_core::server::configuration::Opt;
+use pingora_core::server::Server;
+use pingora_core::upstreams::peer::HttpPeer;
+use pingora_core::Result;
+use pingora_http::ResponseHeader;
+use pingora_proxy::{ProxyHttp, Session};
+
+fn check_login(req: &pingora_http::RequestHeader) -> bool {
+    // implement you logic check logic here
+    req.headers.get("Authorization").map(|v| v.as_bytes()) == Some(b"password")
+}
+
+pub struct MyGateway {
+    req_metric: prometheus::IntCounter,
+}
 
 #[async_trait]
-impl ProxyHttp for LB {
+impl ProxyHttp for MyGateway {
     type CTX = ();
-    fn new_ctx(&self) -> () {
-        ()
+    fn new_ctx(&self) -> Self::CTX {}
+
+    async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool> {
+        if session.req_header().uri.path().starts_with("/login")
+            && !check_login(session.req_header())
+        {
+            let _ = session.respond_error(403).await;
+            // true: early return as the response is already written
+            return Ok(true);
+        }
+        Ok(false)
     }
 
-    async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
-        let upstream = self
-            .0
-            .select(b"", 256) // hash doesn't matter for round robin
-            .unwrap();
+    async fn upstream_peer(
+        &self,
+        session: &mut Session,
+        _ctx: &mut Self::CTX,
+    ) -> Result<Box<HttpPeer>> {
+        let addr = if session.req_header().uri.path().starts_with("/family") {
+            ("1.0.0.1", 443)
+        } else {
+            ("1.1.1.1", 443)
+        };
 
-        println!("upstream peer is: {:?}", upstream);
+        info!("connecting to {addr:?}");
 
-        // Set SNI to one.one.one.one
-        let peer = Box::new(HttpPeer::new(upstream, true, "one.one.one.one".to_string()));
+        let peer = Box::new(HttpPeer::new(addr, true, "one.one.one.one".to_string()));
         Ok(peer)
     }
 
-    async fn upstream_request_filter(
+    async fn response_filter(
         &self,
         _session: &mut Session,
-        upstream_request: &mut RequestHeader,
+        upstream_response: &mut ResponseHeader,
         _ctx: &mut Self::CTX,
-    ) -> Result<()> {
-        upstream_request
-            .insert_header("Host", "one.one.one.one")
+    ) -> Result<()>
+    where
+        Self::CTX: Send + Sync,
+    {
+        // replace existing header if any
+        upstream_response
+            .insert_header("Server", "MyGateway")
             .unwrap();
+        // because we don't support h3
+        upstream_response.remove_header("alt-svc");
+
         Ok(())
+    }
+
+    async fn logging(
+        &self,
+        session: &mut Session,
+        _e: Option<&pingora_core::Error>,
+        ctx: &mut Self::CTX,
+    ) {
+        let response_code = session
+            .response_written()
+            .map_or(0, |resp| resp.status.as_u16());
+        info!(
+            "{} response code: {response_code}",
+            self.request_summary(session, ctx)
+        );
+
+        self.req_metric.inc();
     }
 }
 
-pub struct LB(Arc<LoadBalancer<RoundRobin>>);
-
+// RUST_LOG=INFO cargo run --example load_balancer
+// curl 127.0.0.1:6191 -H "Host: one.one.one.one"
+// curl 127.0.0.1:6190/family/ -H "Host: one.one.one.one"
+// curl 127.0.0.1:6191/login/ -H "Host: one.one.one.one" -I -H "Authorization: password"
+// curl 127.0.0.1:6191/login/ -H "Host: one.one.one.one" -I -H "Authorization: bad"
+// For metrics
+// curl 127.0.0.1:6192/
 fn main() {
-    let mut my_server = Server::new(None).unwrap();
+    env_logger::init();
+
+    // read command line arguments
+    let opt = Opt::from_args();
+    let mut my_server = Server::new(Some(opt)).unwrap();
     my_server.bootstrap();
 
-    let upstreams = LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443"]).unwrap();
+    let mut my_proxy = pingora_proxy::http_proxy_service(
+        &my_server.configuration,
+        MyGateway {
+            req_metric: register_int_counter!("reg_counter", "Number of requests").unwrap(),
+        },
+    );
+    my_proxy.add_tcp("0.0.0.0:6191");
+    my_server.add_service(my_proxy);
 
-    let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
-    lb.add_tcp("0.0.0.0:6188");
+    let mut prometheus_service_http =
+        pingora_core::services::listening::Service::prometheus_http_service();
+    prometheus_service_http.add_tcp("127.0.0.1:6192");
+    my_server.add_service(prometheus_service_http);
 
-    my_server.add_service(lb);
-
-    my_server.run_forever()
+    my_server.run_forever();
 }
